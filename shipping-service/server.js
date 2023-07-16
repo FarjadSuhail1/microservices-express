b0d1fbf677808f529a8a3e49930e4b6b19f4f750
Farjad Suhail <farjad.suhail@systemsltd.com>
2023-07-16 19:31:43 +0500

    first commit

---
+++ shipping-service/server.js
@@ -0,0 +1,49 @@
+// imports
+const express = require("express");
+const morgan = require("morgan");
+
+// init express app
+const app = express();
+
+// use morgan middleware
+app.use(morgan("combined"));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+// ! SHIPPING OPERATIONS
+app.get("/shipping", (req, res) => {
+  res.send("GET SHIPPING");
+});
+
+app.post("/shipping", async (req, res) => {
+  const requestData = req.body;
+
+  fetch('http://localhost:5009/billing', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(requestData),
+  })
+    .then(response => response.json())
+    .then(data => {
+      // console.log('Response:', data);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      // Handle any errors
+    });
+});
+
+app.put("/shipping", (req, res) => {
+  res.send("PUT SHIPPING");
+});
+
+app.delete("/shipping", (req, res) => {
+  res.send("DELETE SHIPPING");
+});
+
+app.listen(5001);
